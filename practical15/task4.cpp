a4793c5de5003b3c45d544f902338e2434748abe
Niharika-012 <87805219+Niharika-012@users.noreply.github.com>
2021-12-28 14:48:27 +0530

    Create task4.cpp
    manipulators

---
+++ practical15/task4.cpp
@@ -0,0 +1,20 @@
+// Manipulators
+
+#include<iostream>
+#include<istream>
+#include<ostream>
+#include<string>
+#include<sstream>
+
+using namespace std; 
+
+int main()
+{
+
+  cout<<"ABC, ";
+  cout<<"EFG"<<endl;
+  cout<<"HIJ "<<ends;
+  cout<<"KLM  "<<flush;
+
+return 0;
+}
