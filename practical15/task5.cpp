528534f9bebbdb304f32b1814ec21fdefd8fc874
Niharika-012 <87805219+Niharika-012@users.noreply.github.com>
2021-12-28 14:54:04 +0530

    Create task5.cpp
    Manipulators

---
+++ practical15/task5.cpp
@@ -0,0 +1,18 @@
+// Manipulators
+
+#include<iostream>
+#include<iomanip>
+
+
+using namespace std; 
+
+int main()
+{
+  
+ double  f = 3.14159;
+
+ cout<<setprecision(5)<<f<<'\n';
+ cout<<setw(8)<<21<<endl;
+
+return 0;
+}
