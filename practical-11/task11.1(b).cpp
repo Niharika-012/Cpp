3696309a725a683c3c81b7eaca2e60ea27bf6eaa
Niharika-012 <87805219+Niharika-012@users.noreply.github.com>
2022-01-07 17:01:37 +0530

    Update task11.1(b).cpp

--- practical-11/task11.1(b).cpp
+++ practical-11/task11.1(b).cpp
@@ -1,60 +1,29 @@
 #include<iostream>
 using namespace std;
 
+
  class A {
-      
-      public:
-      void display(){
-          cout<<endl;
-          cout<<"This the display function of the base class A\n";
-      }
-
- };
-
-class B : virtual public A{
 
       public:
       void display1(){
-          cout<<"This the display1 function of the child class B\n";
+          cout<<"This the display1 function of the Base class A\n";
       }
 
  };
 
- class C :virtual public A{
+ class B: public A {
 
       public:
       void display2(){
-          cout<<"This the display2 function of the child class C\n";
-      }
-
- };
-
- class D : public B , public C{
-
-      public:
-
-
-      void display3(){
-     
-          cout<<"This the display1 function of the child class D\n";
+           cout<<"This the display2 function of the child class B\n";
       }
 
  };
 
 int main()
 {
-     D obj; // object of child class
-
-     obj.display();
-     cout<<endl;
-
-     obj.display1();
-     cout<<endl;
-
-     obj.display2();
-     cout<<endl;
-
-
+    B obj;
+    cout<<endl;
+    obj.display1();
     return 0;
 }
- 
