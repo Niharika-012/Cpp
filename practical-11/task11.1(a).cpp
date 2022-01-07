85bbaecf6fd8b5e600b78f6cf29c6bd5457ab8bc
Niharika-012 <87805219+Niharika-012@users.noreply.github.com>
2022-01-07 16:54:24 +0530

    Update task11.1(a).cpp

--- practical-11/task11.1(a).cpp
+++ practical-11/task11.1(a).cpp
@@ -2,8 +2,7 @@
 using namespace std;
 
 
-
- class B : public A{
+ class A {
 
       public:
       void display1(){
@@ -12,40 +11,22 @@ using namespace std;
 
  };
 
- class C : public A{
+ class B: public A {
 
       public:
       void display2(){
-          cout<<"This the display2 function of the child class C\n";
-      }
 
- };
+         A :: display1();
+        cout<<endl;
 
- class D : public B , public C{
-
-      public:
-      void display3(){
-       
-
-       
-          B::display1();
-          cout<<endl;
-       
-          C::display2();
-          cout<<endl;
-       
-          cout<<"This the display1 function of the child class D\n";
+        cout<<"This the display2 function of the child class C\n";
       }
 
  };
 
 int main()
 {
- 
- D obj;
- 
- obj.display3();
-  
-  return 0; 
- 
+    B obj;
+    obj.display2();
+    return 0;
 }
