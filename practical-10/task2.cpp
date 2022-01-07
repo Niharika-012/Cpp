2030c61dc0f0974a0e068cd77d71d55063e38af2
Niharika-012 <87805219+Niharika-012@users.noreply.github.com>
2022-01-07 13:12:54 +0530

    Create task2.cpp
    pointer to object in cpp

---
+++ practical-10/task2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class student
+{
+    public:
+    string name;
+    int roll;
+    char sec;
+};
+	
+int main()
+	
+{
+    student s1;
+    student *ptr;
+    ptr = &s1;
+    cout << "Enter name of student:- ";
+    cin >> ptr->name;
+    cout << "Enter  roll no.:- ";
+    cin >> ptr->roll;
+    cout << "Enter section of student:- ";
+    cin >> ptr->sec;
+    cout << endl;
+    cout << "Enter name of student:- ";
+    cout << ptr->name << endl;
+    cout << "Enter  roll no. :- ";
+    cout << ptr->roll << endl;
+    cout << "Enter section of student:- ";
+    cout << ptr->sec <<endl;
+	    
+  return 0;
+}
